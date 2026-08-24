5a2c3c3ad8f0cc9cf8e8f48f1fe26546ee0218c4
vitordanielmarques <estudo.marques1809@gmail.com>
2026-08-24 16:36:04 -0400

    Update 02-crescente.js

--- 02-crescente.js
+++ 02-crescente.js
@@ -3,4 +3,43 @@
  uma mensagem ao usuário, informando estes números 
  em ordem crescente.*/
 
+ //usando o método sort() do JavaScript
+ /*
+ let numeros = [8, 5, 2];
+
+ function ordenacaoCrescente(numeros){
+    numeros.sort(function(a,b){
+        return a - b;
+    });
+    return `Os numeros em ordem crescente são: ${numeros}`;
+ }
+
+ console.log(ordenacaoCrescente(numeros));
+*/
+
  
+
+ //usando o método bubble sort
+
+ let vetor = [8, 5, 2];
+
+function ordenarBubble(vetor) {
+
+    for (let i = 0; i < vetor.length - 1; i++) {
+
+        for (let j = 0; j < vetor.length - 1 - i; j++) {
+
+            if (vetor[j] > vetor[j + 1]) {
+
+                let auxiliar = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = auxiliar;
+
+            }
+        }
+    }
+
+    return `Os números em ordem crescente são: ${vetor}`;
+}
+
+console.log(ordenarBubble(vetor));
