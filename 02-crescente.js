08b61b5b7c25e6828ab878143065efe17b327160
vitordanielmarques <estudo.marques1809@gmail.com>
2026-08-24 13:11:55 -0400

    Create 02-crescente.js

---
+++ 02-crescente.js
@@ -0,0 +1,6 @@
+/* Defina um vetor com 3 valores inteiros e atribua valores a eles.
+ Envie-o a uma função, que deve fazer comparações e retornar 
+ uma mensagem ao usuário, informando estes números 
+ em ordem crescente.*/
+
+ 
